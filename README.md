e77ff4a12d74831be4433da558df0bec7f02b0e3
Tosin Oseni <oluwatosinoseni@gmail.com>
2025-01-30 16:31:24 -0700

    .

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MEGN540
 Code repository for helper files and template code to assist with mechatronics class labs.
+
+
+Compile Lufa
+
+LUFA/Documentation/html/index.html
--- c_lib/SerialIO.c
+++ c_lib/SerialIO.c
@@ -75,7 +75,11 @@ static void _USB_Read_Data()
     // will need to adjust to make it non blocking. You'll need to dig into the library to understand
     // how the function above is working then interact at a slightly lower level, but still higher than
     // register level.
-    
+
+    if( USB_DeviceState != DEVICE_STATE_Configured )
+        return;
+
+    Endpoint_SelectEndpoint( CDC_RX_EPADDR );
     if( Endpoint_IsOUTReceived()){
         rb_push_back_B( &_usb_receive_buffer, Endpoint_Read_8());
         Endpoint_Discard_8();
@@ -95,7 +99,11 @@ static void _USB_Write_Data()
     // how the function above is working then interact at a slightly lower level, but still higher than
     // register level.
     //if( Endpoint_IsINReady() && rb_length_B( &_usb_send_buffer ) != 0 )
-    Endpoint_Write_8(rb_pop_front_B( &_usb_send_buffer);) 
+    if( USB_DeviceState != DEVICE_STATE_Configured )
+        return;
+    Endpoint_SelectEndpoint( CDC_TX_EPADDR );
+
+    Endpoint_Write_8(rb_pop_front_B( &_usb_send_buffer)); 
     
 
 }
@@ -241,9 +249,11 @@ void USB_Send_Msg( char* format, char cmd, void* p_data, uint8_t data_len )
     //      usb_send_byte <-- cmd
     //      usb_send_data <-- p_data
     // FUNCTION END
-    uint8_t format_length = 0;
-    for( ; format[format_length] != '\0'; format_length++ );
-    uint8_t msg_length = 1 + format_length + data_len;
+
+
+    // uint8_t format_length = 0;
+    // for( ; format[format_length] != '\0'; format_length++ );
+    // uint8_t msg_length = 1 + format_length + data_len;
 
 
 
@@ -270,7 +280,7 @@ uint8_t USB_Msg_Peek()
     // *** MEGN540  ***
     // YOUR CODE HERE
     // This should only interface with the ring buffers and use your ring buffer functions.
-    return rb_get_B( &_usb_receive_buffer, _usb_receive_buffer->start_index );
+    return rb_get_B( &_usb_receive_buffer, _usb_receive_buffer.start_index );
 }
 
 /**
